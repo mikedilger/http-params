03bd2de3c2c4e22fcc09987937d831e464b6ec32
Mike Dilger <mike@optcomp.nz>
2017-09-25 10:51:46 +1300

    Initial code

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,19 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+// Copyright 2017 Michael Dilger
+// See COPYRIGHT file for details
+
+extern crate url;
+extern crate hyper;
+
+use hyper::server::Request;
+
+pub fn get_query_param<'a>(param: &'a str, request: &Request) -> Option<String>
+{
+    if let Some(q) = request.query() {
+        if let Some(v) = ::url::form_urlencoded::parse(q.as_bytes())
+            .find(|&(ref n,_)| n==param)
+        {
+            return Some(v.1.into_owned())
+        }
     }
+    None
 }
